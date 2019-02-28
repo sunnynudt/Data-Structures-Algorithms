b7b81c19ebdd64ee793ac3f61e7973d4d834470b
zzj <995783795@qq.com>
2019-02-28 08:44:44 +0800

    feat: update dp MaximumSubarrya.js;

--- LeetCode/53_MaximumSubarray/MaximumSubarray.js
+++ LeetCode/53_MaximumSubarray/MaximumSubarray.js
@@ -3,27 +3,13 @@ class MaxSubArray {
     this.list = list
   }
 
-  // https://leetcode.com/problems/maximum-subarray/discuss/139218/Javascript-very-clear-and-short-DP-solution
-  MaxSubArray1() {
-    for (let i = 1; i < this.list.length; i++) {
-      this.list[i] = Math.max(this.list[i], this.list[i] + this.list[i - 1])
-    }
-    // console.log(this.list) // [ -2, 1, -2, 4, 3, 5, 6, 1, 5 ]
-    return Math.max(...this.list)
-  }
-
-  // https://leetcode.com/problems/maximum-subarray/discuss/20193/DP-solution-and-some-thoughts
-  MaxSubArray2() {
-    const n = this.list.length
+  getMaxSubArray() {
     let dp = []
     dp[0] = this.list[0]
-    let max = dp[0]
-    for (let i = 1; i < n; i++) {
-      dp[i] = this.list[i] + (dp[i - 1] > 0 ? dp[i - 1] : 0)
-      max = Math.max(max, dp[i])
+    for (let i = 1; i < this.list.length; i++) {
+      dp[i] = Math.max(this.list[i], this.list[i] + dp[i - 1])
     }
-    // console.log(dp) // [ -2, 1, -2, 4, 3, 5, 6, 1, 5 ]
-    return max
+    return Math.max(...dp)
   }
 }
 
--- LeetCode/53_MaximumSubarray/__test__/MaximumSubarray.test.js
+++ LeetCode/53_MaximumSubarray/__test__/MaximumSubarray.test.js
@@ -1,9 +1,6 @@
 const MaxSubArray = require('../MaximumSubarray')
 
 test('test: ', () => {
-  const list = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
-  const MaxSubArray1 = new MaxSubArray([...list])
-  const MaxSubArray2 = new MaxSubArray([...list])
-  expect(MaxSubArray1.MaxSubArray1()).toBe(6)
-  expect(MaxSubArray2.MaxSubArray2()).toBe(6)
+  const maxSubArray = new MaxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4])
+  expect(maxSubArray.getMaxSubArray()).toBe(6)
 })
