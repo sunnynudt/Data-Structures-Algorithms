06f9c92c273cc167510cedcf86ebe10dcc2edf9d
zzj <995783795@qq.com>
2019-03-04 13:33:34 +0800

    docs: update dp;

--- LeetCode/198_HouseRobber/__test__/HouseRobber.test.js
+++ LeetCode/198_HouseRobber/__test__/HouseRobber.test.js
@@ -6,7 +6,7 @@ test('house robber test1: ', () => {
   expect(houseRobber.getMaxAmount(list.length)).toBe(12)
 })
 
-test('house robber test2:', () => {
+test('house robber test2: ', () => {
   const list = [3, 1, 2, 5]
   const houseRobber = new HouseRobber(list)
   expect(houseRobber.getMaxAmount(list.length)).toBe(8)
