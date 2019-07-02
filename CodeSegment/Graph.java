097f4795eb9b9d3812afd15af2e26697a4486ec8
zzj <995783795@qq.com>
2019-07-02 10:08:21 +0800

    feat: init 图

---
+++ CodeSegment/Graph.java
@@ -0,0 +1,89 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 使用无向图为例，实现广度优先搜索（BFS）和深度优先搜索（DFS）算法
+ */
+public class Graph { // 无向图
+  private int v; // 顶点的个数
+  private LinkedList<Integer> adj[]; // 邻接表
+  boolean found = false; // 全局变量或者类成员变量
+
+  public Graph(int v) {
+    this.v = v;
+    adj = new LinkedList[v];
+    for (int i = 0; i < v; ++i) {
+      adj[i] = new LinkedList<>();
+    }
+  }
+
+  public void addEdge(int s, int t) { // 无向图一条边存2次
+    adj[s].add(t);
+    adj[t].add(s);
+  }
+
+  public void bfs(int s, int t) {
+    if (s == t) {
+      return;
+    }
+    boolean[] visited = new boolean[v];
+    visited[s] = true;
+    Queue<Integer> queue = new LinkedList<>();
+    queue.add(s);
+    int[] prev = new int[v];
+    for (int i = 0; i < v; ++i) {
+      prev[i] = -1;
+    }
+    while (queue.size() != 0) {
+      int w = queue.poll();
+      for (int i = 0; i < adj[w].size(); ++i) {
+        int q = adj[w].get(i);
+        if (!visited[q]) {
+          prev[q] = w;
+          if (q == t) {
+            print(prev, s, t);
+            return;
+          }
+          visited[q] = true;
+          queue.add(q);
+        }
+      }
+    }
+  }
+
+  private void print(int[] prev, int s, int v) { // 递归打印s->t的路径
+    if (prev[t] != -1 && t != s) {
+      print(prev, s, prev[t]);
+    }
+    System.out.print(t + " ");
+  }
+
+  public void dfs(int s, int t) {
+    found = false;
+    boolean[] visited = new boolean[v];
+    int[] prev = new int[v];
+    for (int i = 0; i < v; ++i) {
+      prev[i] = -1;
+      recurDfs(s, t, visited, prev);
+      print(prev, s, t);
+    }
+  }
+
+  private void recurDfs(int w, int t, boolean[] visited, int[] prev) {
+    if (found == true) {
+      return;
+    }
+    visited[w] = true;
+    if (w == t) {
+      found = true;
+      return;
+    }
+    for (int i = 0; i < adj[w].size(); ++i) {
+      int q = adj[w].get(i);
+      if (!visited[q]) {
+        prev[q] = w;
+        recurDfs(w, t, visited, prev);
+      }
+    }
+  }
+}
