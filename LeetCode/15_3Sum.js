4137973059eca33871ed0bb1b708a3af2a5d25a6
zhangzhongjiang <995783795@qq.com>
2019-08-30 22:36:41 +0800

    Update 3Sum

--- LeetCode/15_3Sum.js
+++ LeetCode/15_3Sum.js
@@ -7,38 +7,37 @@ const threeSum = function(nums) {
     return []
   }
 
-  const res = []
   nums.sort((a, b) => a - b)
 
-  for (let i = 0; i < nums.length; i++) {
-    if (i > 0 && nums[i - 1] === nums[i]) {
-      continue
-    }
+  let res = []
 
-    let left = i
+  for (let i = 0; i < nums.length - 2; i++) {
+    if (nums[i] > 0) break
+
+    if (i > 0 && nums[i] === nums[i - 1]) continue
+
+    let left = i + 1
     let right = nums.length - 1
 
     while (left < right) {
-      if (left === i) {
-        left++
-      } else if (right === i) {
-        right--
-      } else if (nums[left] + nums[right] + nums[i] === 0) {
+      if (nums[right] < 0) break
+
+      let sum = nums[i] + nums[left] + nums[right]
+
+      if (sum === 0) {
         res.push([nums[i], nums[left], nums[right]])
 
-        while (nums[left] === nums[left + 1]) {
+        while (left < right && nums[left] === nums[left + 1]) {
           left++
         }
 
-        left++
-
-        while (nums[right] === nums[right - 1]) {
+        while (left < right && nums[right] === nums[right - 1]) {
           right--
         }
 
+        left++
         right--
-        continue
-      } else if (nums[i] + nums[left] + nums[right] > 0) {
+      } else if (sum > 0) {
         right--
       } else {
         left++
