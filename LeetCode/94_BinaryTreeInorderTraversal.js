5a11a7f506aeac3a163fa0f0e18797d795a29f33
zhangzhongjiang <995783795@qq.com>
2019-09-01 17:58:33 +0800

    Add 二叉树中序遍历，利用栈实现，非递归方式

--- LeetCode/94_BinaryTreeInorderTraversal.js
+++ LeetCode/94_BinaryTreeInorderTraversal.js
@@ -5,7 +5,41 @@
  *     this.left = this.right = null;
  * }
  */
+
 /**
+ * TODO: 二叉树的遍历，考核重点是非递归实现
+ * 基于栈，迭代实现
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function(root) {
+  if (root === null) return []
+
+  let stask = [root]
+  let result = []
+  let popItem = null
+  let left = root.left
+
+  while (left) {
+    stask.push(left)
+    left = left.left
+  }
+
+  while ((popItem = stask.pop())) {
+    result.push(popItem.val)
+    let cur = popItem.right
+
+    while (cur) {
+      stask.push(cur)
+      cur = cur.left
+    }
+  }
+
+  return result
+}
+
+/**
+ * 如果数据规模较小，建议使用递归实现
  * 递归写法
  * @param {TreeNode} root
  * @return {number[]}
@@ -18,10 +52,3 @@ var inorderTraversal = function(root) {
 
   return [...left, root.val, ...right]
 }
-
-/**
- * TODO:非递归写法，用栈来实现
- * @param {TreeNode} root
- * @return {number[]}
- */
-var inorderTraversal = function(root) {}
