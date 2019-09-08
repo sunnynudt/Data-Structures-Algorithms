76cfd83c503fd19a6bd1a9477da498dbd7a0c3bb
zhangzhongjiang <995783795@qq.com>
2019-09-08 11:10:24 +0800

    Update LeetCode 使用双指针和dummyHead实现“删除链表的倒数第 N 个节点”

--- LeetCode/19_RemoveNthNodeFromEndofList.js
+++ LeetCode/19_RemoveNthNodeFromEndofList.js
@@ -6,42 +6,29 @@
  * }
  */
 /**
+ * 双指针法
+ * 时间复杂度O(L)
+ * 空间复杂度O(1)
  * @param {ListNode} head
  * @param {number} n
  * @return {ListNode}
  */
 var removeNthFromEnd = function(head, n) {
-  if (head === null) return head
+  let dummyHead = new ListNode(0) // 哑节点
+  dummyHead.next = head
+  let first = dummyHead
+  let second = dummyHead
 
-  if (n <= 0) return head
-
-  let cur = head
-  let cur2 = head
-  let target = head
-  let prev = head
-  let index = 0
-
-  while (cur) {
-    if (index === n) {
-      target = cur
-      break
-    }
-
-    cur = cur.next
-    index++
+  for (let i = 1; i <= n + 1; i++) {
+    first = first.next
   }
 
-  if (cur === null) {
-    return head.next
+  while (first !== null) {
+    first = first.next
+    second = second.next
   }
 
-  while (target) {
-    prev = cur2
-    cur2 = cur2.next
-    target = target.next
-  }
+  second.next = second.next.next
 
-  prev.next = cur2.next
-
-  return head
+  return dummyHead.next
 }
