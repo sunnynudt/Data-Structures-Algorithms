2fe0491ec7728d8cad9376be34e93d8b9c8f9749
zhangzhongjiang <995783795@qq.com>
2019-09-21 09:59:22 +0800

    :zap: Add 矩阵快速幂计算斐波拉契数列，时间复杂度O(logn)

--- LeetCode/509_FibonacciNumber.js
+++ LeetCode/509_FibonacciNumber.js
@@ -1,11 +1,46 @@
 /**
- * 使用二维矩阵计算
+ * 使用二维矩阵计算，矩阵快速幂
  * 时间复杂度O(logn)
  * @param {number} N
  * @return {number}
  */
-fib = N => {
-  // TODO:
+var fib = function(N) {
+  if (N <= 1) return N
+
+  const multi = (a, b) => {
+    let tmp = []
+
+    for (let i = 0; i < 2; i++) {
+      tmp[i] = new Array(2)
+      for (let j = 0; j < 2; j++) {
+        tmp[i][j] = a[i][0] * b[0][j] + a[i][1] * b[1][j]
+      }
+    }
+
+    return tmp
+  }
+
+  const power = N => {
+    const base = [[1, 1], [1, 0]]
+    let matrix
+
+    if (N === 1) {
+      matrix = base
+    } else if (N % 2 === 0) {
+      matrix = power(N / 2)
+      matrix = multi(matrix, matrix)
+    } else if (N % 2 === 1) {
+      matrix = power((N - 1) / 2)
+      matrix = multi(matrix, matrix)
+      matrix = multi(matrix, base)
+    }
+
+    return matrix
+  }
+
+  const ans = power(N)
+
+  return ans[1][0]
 }
 
 /**
